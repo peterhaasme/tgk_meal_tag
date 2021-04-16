86d2b062a4bd5dda30baec6587bd8f2fc355c295
Peter Haas <peter@peterhaas.me>
2021-04-16 10:45:56 -0400

    changed bootstrap theme

--- dash_app.py
+++ dash_app.py
@@ -1,16 +1,16 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+external_stylesheets = [dbc.themes.UNITED]
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 app.layout = html.Div([
-    html.H6("Change the value in the text box to see callbacks in action!"),
-    html.Div(["Input: ",
-              dcc.Input(id='my-input', value='initial value', type='text')]),
+    html.H6("Enter Ingredients:"),
+    html.Div([dcc.Input(id='my-input', value='initial value', type='text')]),
     html.Br(),
     html.Div(id='my-output'),
 
